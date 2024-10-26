8abd82763713cc87a010a1e9f8afb8b21b26e1b2
angie11-11 <147684602+angie11-11@users.noreply.github.com>
2024-10-26 02:41:18 -0700

    Update SortingStrategy.java

--- Code/SortingStrategy.java
+++ Code/SortingStrategy.java
@@ -1,5 +1,17 @@
 import java.util.List;
 
+/**
+ * SortingStrategy is an interface for sorting strategies that can be applied 
+ * to a collection of photos. It provides a method to sort a list of Photo objects
+ * based on a specific criterion, such as name, date, or size.
+ */
 public interface SortingStrategy {
+
+    /**
+     * Sorts the provided list of photos according to the implemented sorting strategy.
+     *
+     * @param photos the list of Photo objects to be sorted
+     * @return a sorted list of Photo objects
+     */
     List<Photo> sort(List<Photo> photos);
 }
