2525da602689bcb95ece665bdd7a71db544525ae
angie11-11 <147684602+angie11-11@users.noreply.github.com>
2024-10-26 02:41:47 -0700

    Update SortByName.java

--- Code/SortByName.java
+++ Code/SortByName.java
@@ -2,7 +2,18 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * SortByName is a concrete implementation of the SortingStrategy interface
+ * that sorts a list of photos based on their name in ascending alphabetical order.
+ */
 public class SortByName implements SortingStrategy {
+
+    /**
+     * Sorts the provided list of photos by name in ascending alphabetical order.
+     *
+     * @param photos the list of Photo objects to be sorted by name
+     * @return a list of Photo objects sorted by their name
+     */
     @Override
     public List<Photo> sort(List<Photo> photos) {
         Collections.sort(photos, Comparator.comparing(Photo::getName));
