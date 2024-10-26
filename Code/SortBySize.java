d53cfe2525a12c59fe404ce544a0f06d1b4f1a16
angie11-11 <147684602+angie11-11@users.noreply.github.com>
2024-10-26 02:41:33 -0700

    Update SortBySize.java

--- Code/SortBySize.java
+++ Code/SortBySize.java
@@ -2,7 +2,18 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * SortBySize is a concrete implementation of the SortingStrategy interface
+ * that sorts a list of photos based on their file size, in ascending order.
+ */
 public class SortBySize implements SortingStrategy {
+
+    /**
+     * Sorts the provided list of photos by file size in ascending order.
+     *
+     * @param photos the list of Photo objects to be sorted by file size
+     * @return a list of Photo objects sorted by their file size
+     */
     @Override
     public List<Photo> sort(List<Photo> photos) {
         Collections.sort(photos, Comparator.comparingLong(Photo::getFileSize));
